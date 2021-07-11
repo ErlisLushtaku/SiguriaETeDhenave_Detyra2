16ab6be51f2901314f481705c1798e1aeadd156f
donats1n <donat.sinani@student.uni-pr.edu>
2021-07-11 21:08:56 +0200

    Test

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace DataSecurity_pr2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = 4;
+            
+        }
     }
 }
