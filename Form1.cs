02e1d8557c2338a7dac9bd9018dd37700e04b15d
Erlis Lushtaku <erlis.lushtaku@student.uni-pr.edu>
2021-07-22 01:49:34 +0200

    request to server functionality

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,7 @@ namespace DataSecurity_pr2
 {
     public partial class Form1 : Form
     {
+        ClientSide client;
         public Form1()
         {
             InitializeComponent();
@@ -21,17 +22,20 @@ namespace DataSecurity_pr2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           
-            
+            client = new ClientSide();
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            this.Hide();
-            Form3 form = new Form3();
-            form.ShowDialog();
-            this.Close();
+            if(textBox1.Text!="" && textBox2.Text!="")
+            {
+                client.requestToServer("login-" + textBox1.Text + ">" + textBox2.Text);
 
+                this.Hide();
+                Form3 form = new Form3();
+                form.ShowDialog();
+                this.Close();
+            }
         }
 
         private void label2_Click(object sender, EventArgs e)
