62e0a63e26ed7cfc2eba083f4da80bad49f7b6a7
donats1n <donat.sinani@student.uni-pr.edu>
2021-07-22 11:14:18 +0200

    added new certificate

--- ServerSide.cs
+++ ServerSide.cs
@@ -19,17 +19,16 @@ namespace Siguri_Projekti2
 {
     class ServerSide
     {
-       public static X509Certificate2 certifikata = new X509Certificate2("../../Siguri_Projekti2.cer", "123456");
-        
+        public static X509Certificate2 certifikata = new X509Certificate2("../../Siguri_Projekti2.cer", "123456");
+
         private const String secret = "enesh";
         private DESCryptoServiceProvider des;
-        private RSACryptoServiceProvider rsa;
-        static byte[] DesKey;
-        private Socket server;
+        private RSACryptoServiceProvider rsa;      
 
-        public string Encrypt(string response) {
+        public string Encrypt(string response)
+        {
             byte[] byteResponse = Encoding.UTF8.GetBytes(response);
-             des = new DESCryptoServiceProvider();
+            des = new DESCryptoServiceProvider();
             //  des.Key = DesKey;
             des.GenerateKey();
             des.Mode = CipherMode.CBC;
@@ -37,20 +36,25 @@ namespace Siguri_Projekti2
             des.GenerateIV(); //gjenerimi i IV`
             byte[] IV = des.IV;
             MemoryStream ms = new MemoryStream();
-            CryptoStream cs = new CryptoStream(ms,des.CreateEncryptor(),CryptoStreamMode.Write);
-            cs.Write(byteResponse,0, byteResponse.Length);
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(byteResponse, 0, byteResponse.Length);
             cs.Close();
             byte[] encryptedResponse = ms.ToArray();
             byte[] concatenatedResponse = IV.Concat(encryptedResponse).ToArray();
             return Convert.ToBase64String(concatenatedResponse);
-            
+
         }
-        public string Decrypt(string clientMessage) {
-            string[] explodedMessages = clientMessage.Split(',');
-            byte[] IV = Convert.FromBase64String(explodedMessages[0]);
-            byte[] enDesKey = Convert.FromBase64String(explodedMessages[1]);
-            byte[] enMessage = Convert.FromBase64String(explodedMessages[2]);
-            byte[] desKey = rsa.Decrypt(enDesKey,false);
+        public string Decrypt(string clientMessage)
+        {
+            byte[] fullMsgData = Convert.FromBase64String(clientMessage);
+ 
+            byte[] IV = new byte[8];
+            byte[] enDesKey = new byte[128];
+            byte[] enMessage = new byte[fullMsgData.Length - IV.Length - enDesKey.Length];
+            Array.Copy(fullMsgData, IV, 8);
+            Array.Copy(fullMsgData, IV.Length, enDesKey, 0, 128);
+            Array.Copy(fullMsgData, IV.Length + enDesKey.Length, enMessage, 0, fullMsgData.Length - IV.Length - enDesKey.Length);                       
+            byte[] desKey = rsa.Decrypt(enDesKey, false);
             DES des = DES.Create();
             des.IV = IV;
             des.Key = desKey;
@@ -67,84 +71,79 @@ namespace Siguri_Projekti2
             // login-...
         }
 
-        public void createResponseToUser(Socket user) {
-            while (true)
+        public void createResponseToUser(UdpClient user,string data)
+        {           
+            string plainData = Decrypt(data);
+            string logOrRegOrBill = plainData.Split('-')[0];
+            string command = plainData.Split('-')[1];
+
+            //komanda per login=emaili,pw
+            //per register=emri,mbi,imella,id,pw
+            //per fatura=id,viti,muji,...,...
+            //login-command
+            switch (logOrRegOrBill)
             {
-                byte[] bytes = new Byte[1024];
-                string data = null;
-                int numBytes = user.Receive(bytes);
-                data += Encoding.UTF8.GetString(bytes, 0, numBytes);
+                case "login":
+                    string emaili = command.Split('>')[0];
+                    if (UserRepository.findUser(emaili) == null)
+                    {
+                        string encryptedResponse = Encrypt("ERROR");
+                        user.Send(Encoding.UTF8.GetBytes(encryptedResponse), Encoding.UTF8.GetBytes(encryptedResponse).Length);
+                    }
+                    else
+                    {
+                        user.Send(Encoding.UTF8.GetBytes(Encrypt(JWTSignature(emaili))), Encoding.UTF8.GetBytes(Encrypt(JWTSignature(emaili))).Length);
+                    }
+                    break;
 
-                string plainData = Decrypt(data);
-                string logOrRegOrBill = plainData.Split('-')[0];
-                string command = plainData.Split('-')[1];
-
-                //komanda per login=emaili,pw
-                //per register=emri,mbi,imella,id,pw
-                //per fatura=id,viti,muji,...,...
-//login-command
-                switch (logOrRegOrBill)
-                {
-                    case "login":
-                        string emaili = command.Split('>')[0];
-                        if (UserRepository.findUser(emaili) == null)
+                case "register":
+                    string userEmail = command.Split('>')[2];
+                    if (UserRepository.findUser(userEmail) == null)
+                    {
+                        string name = command.Split('>')[0];
+                        string surname = command.Split('>')[1];
+                        string email = command.Split('>')[2];
+                        int id = Convert.ToInt32(command.Split('>')[3]);
+                        string password = command.Split('>')[4];
+                        string salt = command.Split('>')[5];
+                        User useri = new User(name, surname, email, id, password, salt);
+                        if (UserRepository.createUser(useri))
                         {
-                            string encryptedResponse = Encrypt("ERROR");
-                            user.Send(Encoding.UTF8.GetBytes(encryptedResponse));
+                            user.Send(Encoding.UTF8.GetBytes(Encrypt("OK")), Encoding.UTF8.GetBytes(Encrypt("OK")).Length);
                         }
                         else
                         {
-                            user.Send(Encoding.UTF8.GetBytes(Encrypt(JWTSignature(emaili))));
+                            byte[] a = new byte[2];
+                            user.Send(Encoding.UTF8.GetBytes(Encrypt("ERROR")), Encoding.UTF8.GetBytes(Encrypt("ERROR")).Length);
+                            
                         }
-                        break;
+                    }
+                    else
+                    {
 
-                    case "register":
-                        string userEmail = command.Split('>')[2];
-                        if (UserRepository.findUser(userEmail) == null)
-                        {
-                            string name = command.Split('>')[0];
-                            string surname = command.Split('>')[1];
-                            string email = command.Split('>')[2];
-                            int id = Convert.ToInt32(command.Split('>')[3]);
-                            string password = command.Split('>')[4];
-                            string salt = command.Split('>')[5];
-                            User useri = new User(name, surname, email, id, password, salt);
-                            if (UserRepository.createUser(useri))
-                            {
-                                user.Send(Encoding.UTF8.GetBytes(Encrypt("OK")));
-                            }
-                            else
-                            {
+                        user.Send(Encoding.UTF8.GetBytes(Encrypt("ERROR")), Encoding.UTF8.GetBytes(Encrypt("ERROR")).Length);
+                    }
+                    break;
 
-                                user.Send(Encoding.UTF8.GetBytes(Encrypt("ERROR")));
-                            }
-                        }
-                        else
-                        {
-
-                            user.Send(Encoding.UTF8.GetBytes(Encrypt("ERROR")));
-                        }
-                        break;
-
-                    case "registerbill":
-                        string type = command.Split('>')[0];
-                        int year = Convert.ToInt32(command.Split('>')[1]);
-                        string month = command.Split('>')[2];
-                        double value = Convert.ToDouble(command.Split('>')[3]);
-                        int userId = Convert.ToInt32(command.Split('>')[4]);
-                        Bill bill = new Bill(type, year, month, value, userId);
-                        BillRepository.addBill(bill);
-                        user.Send(Encoding.UTF8.GetBytes(Encrypt("OK")));
-                        break;
-                    default:
-                        break;
-                }
+                case "registerbill":
+                    string type = command.Split('>')[0];
+                    int year = Convert.ToInt32(command.Split('>')[1]);
+                    string month = command.Split('>')[2];
+                    double value = Convert.ToDouble(command.Split('>')[3]);
+                    int userId = Convert.ToInt32(command.Split('>')[4]);
+                    Bill bill = new Bill(type, year, month, value, userId);
+                    BillRepository.addBill(bill);
+                    user.Send(Encoding.UTF8.GetBytes(Encrypt("OK")), Encoding.UTF8.GetBytes(Encrypt("OK")).Length);
+                    break;
+                default:
+                    break;
             }
-            //user.Shutdown(SocketShutdown.Both);
-            //user.Close();
+
+
         }
-      
-        public static string JWTSignature(string email) {
+
+        public static string JWTSignature(string email)
+        {
             User useri = UserRepository.findUser(email);
 
             IJwtAlgorithm alg = new RS256Algorithm(certifikata);
@@ -167,38 +166,28 @@ namespace Siguri_Projekti2
 
             return signedMessage;
         }
-        public  ServerSide()
+        public ServerSide()
         {
-            try
+            rsa = (RSACryptoServiceProvider)certifikata.PrivateKey;
+            serverThread();
+        }
+        private void ServerMultiThread()
+        {
+            Thread thdUDPServer = new Thread(new
+            ThreadStart(serverThread));
+            thdUDPServer.Start();
+        }
+        public void serverThread()
+        {
+            UdpClient udpClient = new UdpClient(8080);
+            while (true)
             {
-                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
-                IPAddress ipAddr = ipHost.AddressList[0];
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
-                server = new Socket(ipAddr.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
-                server.Bind(localEndPoint);
-                // rsa = (RSACryptoServiceProvider)certifikata.PrivateKey;
-                
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.UTF8.GetString(receiveBytes);                
+                createResponseToUser(udpClient,returnData);           
             }
         }
-        //public void listen()
-        //{
-        //    server.Listen(10);
-
-        //    while (true)
-        //    {
-
-        //        Socket client = server.Accept();
-        //        Thread thread = new Thread(() => this.sendResponseToUser(client));
-        //        thread.Start();
-
-
-        //    }
-        //}
 
     }
-
 }
